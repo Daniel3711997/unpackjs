3b9740aa7a267c54b49d5af782bde3a749a795c8
Onofrei Costinel Danut <onofreicostineldanut12@gmail.com>
2023-03-14 17:51:08 +0200

    updates

--- postcss.config.js
+++ postcss.config.js
@@ -8,12 +8,12 @@ const plugins = [
         'postcss-preset-env',
         {
             stage: 3,
-            autoprefixer: {
-                flexbox: 'no-2009',
-            },
-            features: {
-                'custom-properties': false,
-            },
+            // autoprefixer: {
+            //     flexbox: 'no-2009',
+            // },
+            // features: {
+            //     'custom-properties': false,
+            // },
         },
     ],
 ];
