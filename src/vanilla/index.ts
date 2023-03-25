e177bda2cd613507ab2db806522c8db4b59cfefb
Onofrei Costinel Danut <onofreicostineldanut12@gmail.com>
2023-03-25 13:33:07 +0200

    chore: updated settings and removed helpers

--- src/vanilla/index.ts
+++ src/vanilla/index.ts
@@ -10,10 +10,3 @@ console.log('Hello world');
  * Do not remove this helper function as it is used by the build process
  */
 if (module.hot) module.hot.accept();
-
-const xx = console.log;
-const str = '😜';
-
-for (const s of str) {
-    xx(s);
-}
