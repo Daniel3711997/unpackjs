a1fe6bc62b94be961c5ed0c7853f640fe04656e7
Onofrei Costinel Danut <onofreicostineldanut12@gmail.com>
2023-07-10 17:01:49 +0300

    chore: deps update

--- app.config.js
+++ app.config.js
@@ -150,7 +150,7 @@ module.exports = {
             env: {
                 debug: false,
                 mode: 'usage',
-                coreJs: '3.31.0',
+                coreJs: '3.31.1',
                 targets: isProduction ? pack.browserslist.production : pack.browserslist.development,
             },
         };
